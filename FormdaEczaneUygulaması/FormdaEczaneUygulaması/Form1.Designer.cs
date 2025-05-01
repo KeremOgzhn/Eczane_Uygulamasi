9e6a1c172e3a8cbe53b0696c6ba1ac1b6ccf47c1
Kerem Oğuzhan <keremoguzhan1905@gmail.com>
2025-05-02 00:09:40 +0300

    Eczane_Uygulamasi
    C# formda MSSQL veri tabanı programı kullanarak oluşturduğum program

---
+++ FormdaEczaneUygulaması/FormdaEczaneUygulaması/Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace FormdaEczaneUygulaması
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Button btnGuncelle;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.Button btnFotografSec;
+        private System.Windows.Forms.Button btnTemizle;
+        private System.Windows.Forms.TextBox txtIlacKodu;
+        private System.Windows.Forms.TextBox txtIlacAdi;
+        private System.Windows.Forms.TextBox txtFiyat;
+        private System.Windows.Forms.TextBox txtAdet;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox txtFotografYolu;
+        private System.Windows.Forms.TextBox txtArama;
+        private System.Windows.Forms.Label lblArama;
+        private System.Windows.Forms.Button btnAra;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btnGuncelle = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.btnFotografSec = new System.Windows.Forms.Button();
+            this.btnTemizle = new System.Windows.Forms.Button();
+            this.txtIlacKodu = new System.Windows.Forms.TextBox();
+            this.txtIlacAdi = new System.Windows.Forms.TextBox();
+            this.txtFiyat = new System.Windows.Forms.TextBox();
+            this.txtAdet = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.txtFotografYolu = new System.Windows.Forms.TextBox();
+            this.txtArama = new System.Windows.Forms.TextBox();
+            this.lblArama = new System.Windows.Forms.Label();
+            this.btnAra = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(776, 200);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(12, 396);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(75, 23);
+            this.btnEkle.TabIndex = 1;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // btnGuncelle
+            // 
+            this.btnGuncelle.Location = new System.Drawing.Point(93, 396);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(75, 23);
+            this.btnGuncelle.TabIndex = 2;
+            this.btnGuncelle.Text = "Güncelle";
+            this.btnGuncelle.UseVisualStyleBackColor = true;
+            this.btnGuncelle.Click += new System.EventHandler(this.btnGuncelle_Click);
+            // 
+            // btnSil
+            // 
+            this.btnSil.Location = new System.Drawing.Point(174, 396);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(75, 23);
+            this.btnSil.TabIndex = 3;
+            this.btnSil.Text = "Sil";
+            this.btnSil.UseVisualStyleBackColor = true;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // btnFotografSec
+            // 
+            this.btnFotografSec.Location = new System.Drawing.Point(318, 251);
+            this.btnFotografSec.Name = "btnFotografSec";
+            this.btnFotografSec.Size = new System.Drawing.Size(75, 23);
+            this.btnFotografSec.TabIndex = 4;
+            this.btnFotografSec.Text = "Fotoğraf Seç";
+            this.btnFotografSec.UseVisualStyleBackColor = true;
+            this.btnFotografSec.Click += new System.EventHandler(this.btnFotografSec_Click);
+            // 
+            // btnTemizle
+            // 
+            this.btnTemizle.Location = new System.Drawing.Point(255, 396);
+            this.btnTemizle.Name = "btnTemizle";
+            this.btnTemizle.Size = new System.Drawing.Size(75, 23);
+            this.btnTemizle.TabIndex = 10;
+            this.btnTemizle.Text = "Temizle";
+            this.btnTemizle.UseVisualStyleBackColor = true;
+            this.btnTemizle.Click += new System.EventHandler(this.btnTemizle_Click);
+            // 
+            // txtIlacKodu
+            // 
+            this.txtIlacKodu.Location = new System.Drawing.Point(125, 241);
+            this.txtIlacKodu.Name = "txtIlacKodu";
+            this.txtIlacKodu.Size = new System.Drawing.Size(100, 22);
+            this.txtIlacKodu.TabIndex = 5;
+            // 
+            // txtIlacAdi
+            // 
+            this.txtIlacAdi.Location = new System.Drawing.Point(125, 273);
+            this.txtIlacAdi.Name = "txtIlacAdi";
+            this.txtIlacAdi.Size = new System.Drawing.Size(100, 22);
+            this.txtIlacAdi.TabIndex = 6;
+            // 
+            // txtFiyat
+            // 
+            this.txtFiyat.Location = new System.Drawing.Point(125, 305);
+            this.txtFiyat.Name = "txtFiyat";
+            this.txtFiyat.Size = new System.Drawing.Size(100, 22);
+            this.txtFiyat.TabIndex = 7;
+            // 
+            // txtAdet
+            // 
+            this.txtAdet.Location = new System.Drawing.Point(125, 337);
+            this.txtAdet.Name = "txtAdet";
+            this.txtAdet.Size = new System.Drawing.Size(100, 22);
+            this.txtAdet.TabIndex = 8;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(394, 289);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(200, 140);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+           
+            // 
+            // txtFotografYolu
+            // 
+            this.txtFotografYolu.Location = new System.Drawing.Point(399, 253);
+            this.txtFotografYolu.Name = "txtFotografYolu";
+            this.txtFotografYolu.Size = new System.Drawing.Size(200, 22);
+            this.txtFotografYolu.TabIndex = 11;
+            // 
+            // txtArama
+            // 
+            this.txtArama.Location = new System.Drawing.Point(663, 229);
+            this.txtArama.Name = "txtArama";
+            this.txtArama.Size = new System.Drawing.Size(100, 22);
+            this.txtArama.TabIndex = 12;
+            // 
+            // lblArama
+            // 
+            this.lblArama.AutoSize = true;
+            this.lblArama.Location = new System.Drawing.Point(594, 235);
+            this.lblArama.Name = "lblArama";
+            this.lblArama.Size = new System.Drawing.Size(50, 16);
+            this.lblArama.TabIndex = 13;
+            this.lblArama.Text = "Arama:";
+            // 
+            // btnAra
+            // 
+            this.btnAra.Location = new System.Drawing.Point(663, 272);
+            this.btnAra.Name = "btnAra";
+            this.btnAra.Size = new System.Drawing.Size(75, 23);
+            this.btnAra.TabIndex = 14;
+            this.btnAra.Text = "Ara";
+            this.btnAra.UseVisualStyleBackColor = true;
+            this.btnAra.Click += new System.EventHandler(this.btnAra_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(46, 241);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 16);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "İlaç Kodu";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(57, 275);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 16);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "İlaç Adı";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(72, 311);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 16);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Fiyat";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(73, 343);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 16);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "Adet";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnAra);
+            this.Controls.Add(this.lblArama);
+            this.Controls.Add(this.txtArama);
+            this.Controls.Add(this.txtFotografYolu);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txtAdet);
+            this.Controls.Add(this.txtFiyat);
+            this.Controls.Add(this.txtIlacAdi);
+            this.Controls.Add(this.txtIlacKodu);
+            this.Controls.Add(this.btnTemizle);
+            this.Controls.Add(this.btnFotografSec);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnGuncelle);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Eczane Uygulaması";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
---
+++ FormdaEczaneUygulaması/FormdaEczaneUygulaması/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormdaEczaneUygulaması
+{
+    public partial class Form1 : Form
+    {
+        string connectionString = @"Server=.;Database=eczanedb;Integrated Security=True;";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            VerileriYukle();
+        }
+
+        private void VerileriYukle()
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM ilac", con);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                dataGridView1.DataSource = dt;
+            }
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("INSERT INTO ilac (ilacKodu, ilacAdi, fiyat, adet, fotograf) VALUES (@ilacKodu, @ilacAdi, @fiyat, @adet, @fotograf)", con);
+                cmd.Parameters.AddWithValue("@ilacKodu", txtIlacKodu.Text);
+                cmd.Parameters.AddWithValue("@ilacAdi", txtIlacAdi.Text);
+                cmd.Parameters.AddWithValue("@fiyat", Convert.ToDecimal(txtFiyat.Text));
+                cmd.Parameters.AddWithValue("@adet", Convert.ToInt32(txtAdet.Text));
+                cmd.Parameters.AddWithValue("@fotograf", txtFotografYolu.Text);
+                cmd.ExecuteNonQuery();
+                VerileriYukle();
+                VerileriTemizle();
+            }
+        }
+
+        private void btnGuncelle_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("UPDATE ilac SET ilacKodu = @ilacKodu, ilacAdi = @ilacAdi, fiyat = @fiyat, adet = @adet, fotograf = @fotograf WHERE siraNo = @siraNo", con);
+                cmd.Parameters.AddWithValue("@siraNo", dataGridView1.CurrentRow.Cells[0].Value);
+                cmd.Parameters.AddWithValue("@ilacKodu", txtIlacKodu.Text);
+                cmd.Parameters.AddWithValue("@ilacAdi", txtIlacAdi.Text);
+                cmd.Parameters.AddWithValue("@fiyat", Convert.ToDecimal(txtFiyat.Text));
+                cmd.Parameters.AddWithValue("@adet", Convert.ToInt32(txtAdet.Text));
+                cmd.Parameters.AddWithValue("@fotograf", txtFotografYolu.Text);
+                cmd.ExecuteNonQuery();
+                VerileriYukle();
+                VerileriTemizle();
+            }
+        }
+
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("DELETE FROM ilac WHERE siraNo = @siraNo", con);
+                cmd.Parameters.AddWithValue("@siraNo", dataGridView1.CurrentRow.Cells[0].Value);
+                cmd.ExecuteNonQuery();
+                VerileriYukle();
+                VerileriTemizle();
+            }
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = dataGridView1.Rows[e.RowIndex];
+                txtIlacKodu.Text = row.Cells[1].Value.ToString();
+                txtIlacAdi.Text = row.Cells[2].Value.ToString();
+                txtFiyat.Text = row.Cells[3].Value.ToString();
+                txtAdet.Text = row.Cells[4].Value.ToString();
+                txtFotografYolu.Text = row.Cells[5].Value.ToString();
+                pictureBox1.ImageLocation = txtFotografYolu.Text;
+            }
+        }
+
+        private void btnFotografSec_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                txtFotografYolu.Text = ofd.FileName;
+                pictureBox1.ImageLocation = ofd.FileName;
+            }
+        }
+
+        private void btnTemizle_Click(object sender, EventArgs e)
+        {
+            VerileriTemizle();
+        }
+
+        private void VerileriTemizle()
+        {
+            txtIlacKodu.Clear();
+            txtIlacAdi.Clear();
+            txtFiyat.Clear();
+            txtAdet.Clear();
+            txtFotografYolu.Clear();
+            pictureBox1.Image = null;
+        }
+
+        private void btnAra_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM ilac WHERE ilacAdi LIKE @arama OR ilacKodu LIKE @arama", con);
+                da.SelectCommand.Parameters.AddWithValue("@arama", "%" + txtArama.Text + "%");
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                dataGridView1.DataSource = dt;
+            }
+        }
+
+       
+    }
+}
